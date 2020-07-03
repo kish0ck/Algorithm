ab521dace3e46dd917ffd2376d6a3ba2dc0d47c4
kistone <kistone3@naver.com>
2020-07-03 23:03:26 +0900

    20200703

--- src/개념/LIS.java
+++ src/개념/LIS.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 
 public class LIS {
 
+	/*
+	 * 
+	 * 수열 A가 주어졌을 때, 가장 긴 증가하는 부분 수열
+	 * 예를 들어, 수열 A = {10, 20, 10, 30, 20, 50} 인 경우에 
+	 * 가장 긴 증가하는 부분 수열은 A = {**10**, **20**, 10, **30**, 20, **50**} 이고, 길이는 4이다.
+	 * 
+	 * */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
