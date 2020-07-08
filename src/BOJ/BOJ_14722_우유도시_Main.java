5ad135d40bc36618f9ba1218c2e943d510b58aff
kistone <kistone3@naver.com>
2020-07-08 14:08:48 +0900

    20200708

--- src/BOJ/BOJ_14722_우유도시_Main.java
+++ src/BOJ/BOJ_14722_우유도시_Main.java
@@ -3,8 +3,6 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ_14722_우유도시_Main {
@@ -23,56 +21,65 @@ public class BOJ_14722_우유도시_Main {
 		// 딸기 -> 초코 -> 바나나 -> 딸기
 		// 0 딸기, 1 초코, 2 바나나
 		// r, c, 현재우유, 개수
-		int[] dr = {0,1}, dc= {1,0};
-		int ans = 0;
-		Queue<int[]> q = new LinkedList<int[]>();
-		Queue<boolean[][]> q2 = new LinkedList<boolean[][]>();
+		int[][][] dp = new int[N][N][3];
+		int now_milk = 0;
+		boolean flag = false;
 		if(map[0][0]==0) {
-			q.offer(new int[] {0, 0, 0, 1});
-			boolean[][] visited = new boolean[N][N];
-			visited[0][0] = true;
-			q2.offer(visited);
+			flag = true;
+			now_milk = 1;
+			dp[0][0][0]++;
 		}
-		while(!q.isEmpty()) {
-			int[] cur = q.poll();
-			boolean[][] cur_visited = q2.poll();
-			cur_visited[cur[0]][cur[1]] = true;
-			int now_milk = cur[2];
-			int next_milk = -1;
-			switch(now_milk) {
-				case 0:
-					next_milk=1;
-					break;
-				case 1:
-					next_milk=2;
-					break;
-				case 2:
-					next_milk=0;
-					break;
+		
+		for (int i = 1; i < N; i++) {
+			dp[0][i]=dp[0][i-1].clone();
+			if(map[0][i]==now_milk) {
+				dp[0][i][now_milk]++;
+				now_milk++;
+				now_milk%=3;
 			}
-			int cnt = cur[3];
-			if(ans<cnt) ans=cnt;
-			for (int d = 0; d < 2; d++) {
-				int nr = cur[0] + dr[d];
-				int nc = cur[1] + dc[d];
-				if(nr>-1&&nc>-1&&nr<N&&nc<N&&!cur_visited[nr][nc]) {
-					if(nr==N-1&&nc==N-1) {
-						if(map[N-1][N-1]==next_milk) {
-							if(ans<cnt+1) ans=cnt+1;
-						} 
-					}else {
-						if(map[nr][nc]==next_milk) {
-							q.offer(new int[] {nr,nc,next_milk,cnt+1});
-							q2.offer(cur_visited);
-						}else {
-							q.offer(new int[] {nr,nc,now_milk,cnt});
-							q2.offer(cur_visited);
-						}
-					}
+		}
+		if(flag) {
+			now_milk = 1;
+		}else now_milk = 0;
+		for (int i = 1; i < N; i++) {
+			dp[i][0]=dp[i-1][0].clone();
+			if(map[i][0]==now_milk) {
+				dp[i][0][now_milk]++;
+				now_milk++;
+				now_milk%=3;
+			}
+		}
+		
+		for (int i = 1; i < N; i++) {
+			for (int j = 1; j < N; j++) {
+				int sumR = 0;
+				int sumC = 0;
+				for (int q = 0; q < 3; q++) {
+					sumR+=dp[i-1][j][q];
+					sumC+=dp[i][j-1][q];
 				}
+				
+				if(sumR>=sumC) {
+					dp[i][j]=dp[i-1][j].clone();
+					now_milk=sumR%3;
+				}else if(sumR<sumC) {
+					dp[i][j]=dp[i][j-1].clone();
+					now_milk=sumC%3;
+				}
+				
+				if(map[i][j]==now_milk) {
+					dp[i][j][now_milk]++;
+				}
+				
 			}
+		}
+		
+		int ans = 0;
+		for (int i = 0; i < 3; i++) {
+			ans+=dp[N-1][N-1][i];
 		}
 		System.out.println(ans);
+		
 	}
 
 }
