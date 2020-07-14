061efa742e9019fb0fd8fa8953e9fda19d203bc8
kistone <kistone3@naver.com>
2020-07-14 17:36:20 +0900

    20200714

--- src/BOJ/RE_BOJ_1937_욕심쟁이판다_Main.java
+++ src/BOJ/RE_BOJ_1937_욕심쟁이판다_Main.java
@@ -3,10 +3,10 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class RE_BOJ_1937_욕심쟁이판다_Main {
@@ -15,45 +15,41 @@ public class RE_BOJ_1937_욕심쟁이판다_Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int[][] map = new int[N][N];
+		PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[2]-o2[2];
+			}
+		});
+		
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine()," ");
 			for (int j = 0; j < N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
+				pq.offer(new int[] {i,j,map[i][j]});
+			}
+		}
+		int[][] dp = new int[N][N];
+		int[] dr = {0,0,-1,1};
+		int[] dc = {1,-1,0,0};
+		int ans = 1;
+		
+		while(!pq.isEmpty()) {
+			int[] cur = pq.poll();
+			int r = cur[0];
+			int c = cur[1];
+			dp[r][c] = 1;
+			for (int d = 0; d < 4; d++) {
+				int nr = dr[d] + r;
+				int nc = dc[d] + c;
+				if(nr>-1&&nc>-1&&nr<N&&nc<N&&map[r][c]>map[nr][nc]) {
+					dp[r][c] = Math.max(dp[r][c], dp[nr][nc]+1);
+					if(ans<dp[r][c]) ans=dp[r][c];
+				}
 			}
 		}
 		
-		int[] dr = {0,0,-1,1};
-		int[] dc = {1,-1,0,0};
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				boolean[][] visited = new boolean[N][N];
-				Queue<int[]> q = new LinkedList<int[]>();
-				q.offer(new int[] {i,j,map[i][j]});
-				visited[i][j]=true;
-				int K = 0;
-				while(!q.isEmpty()) {
-					int size = q.size();
-					K++;
-					for (int s = 0; s < size; s++) {
-						int[] cur = q.poll();
-						int r = cur[0];
-						int c = cur[1];
-						int val = cur[2];
-						for (int d = 0; d < 4; d++) {
-							int nr = dr[d]+r;
-							int nc = dc[d]+c;
-							if(nr>-1&&nc>-1&&nr<N&&nc<N&&!visited[nr][nc]&&val<map[nr][nc]) {
-								visited[nr][nc]=true;
-								q.offer(new int[] {nr,nc,map[nr][nc]});
-							}
-						}
-					}
-				}
-				if(K>max) max = K;
-			}
-		}
-		System.out.println(max);
+		System.out.println(ans);
 		
 	}
 
