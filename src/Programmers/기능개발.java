4a39f32cb9e8e18cc9a5dbdf800a24e136521937
kistone <kistone3@naver.com>
2021-08-03 22:56:38 +0900

    2021-08-03 algorithm

--- src/Programmers/기능개발.java
+++ src/Programmers/기능개발.java
@@ -1,6 +1,6 @@
 package Programmers;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class 기능개발 {
 
@@ -9,58 +9,40 @@ public class 기능개발 {
 		int[] speeds = {1,30,5};
 //		int[] progresses = {95, 90, 99, 99, 80, 99};
 //		int[] speeds = {1, 1, 1, 1, 1, 1};
-		System.out.println(Arrays.toString(solution(progresses, speeds)));
+//		System.out.println(Arrays.toString(solution(progresses, speeds)));
 		
 	}
-	public static int[] solution(int[] progresses, int[] speeds) {
-//        int[] answer = {};
-        int len = progresses.length;
-        int[] arr = new int[len];
-        for (int i = 0; i < len; i++) {
-        	if((100-progresses[i])%speeds[i]==0) {
-        		arr[i] = (100-progresses[i])/speeds[i];
-        	}else {
-        		arr[i] = (100-progresses[i])/speeds[i];
-        		arr[i]++;
-        	}
-		}
+	public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        int length = progresses.length;
+        for(int i=0; i<length; i++) {
+            queue.offer((int)Math.ceil((double)(100-progresses[i])/speeds[i]));
+        }
+       
+        List<Integer> list = new ArrayList<Integer>();
         
-        int[] result = new int[len];
-        Arrays.fill(result, -1);
-        int before = arr[0];
-        int val = 1;
-        int idx = 0;
-        for (int i = 1; i < len; i++) {
-			int now = arr[i];
-			if(before>now) {
-				val++;
-			}else {
-				for (int j = 0; j < len; j++) {
-					if(result[j]==-1) {
-						idx++;
-						result[j] = val;
-						val=1;
-						break;
-					}
-				}
-				before=now;
-			}
-		}
-        
-        int[] answer = new int[idx+1];
-        int a = 0;
-        for (int i = 0; i < idx; i++) {
-			answer[i] = result[i];
-			a += answer[i];
-		}
-        answer[idx] = len - a;
-//        int idx = -1;
-//        for (int i = 0; i < len; i++) {
-//        	if(result[i]==-1) {
-//        		idx = i;
-//        	}
-//        }
-        
+        int preValue = queue.poll();
+        int count = 1;
+        int nowValue = 0;
+        while(!queue.isEmpty()){
+            nowValue = queue.poll();
+            if(preValue >= nowValue){
+                count++;
+            }else {
+                preValue = nowValue;
+                list.add(count);
+                count = 1;
+            }
+        }
+       
+        list.add(count);
+       
+        // System.out.println("리스트 사이즈 : " + list.size());
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+            // System.out.println("리스트get(i)"+list.get(i));
+        }
         
         return answer;
     }
