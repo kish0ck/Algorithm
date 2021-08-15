6df29afc4d5d6af2797153be824433af1df42596
kistone <kistone3@naver.com>
2021-08-15 20:25:52 +0900

    2021-08-15 algorithm

---
+++ src/Programmers/문자열을정수로바꾸기.java
@@ -0,0 +1,13 @@
+package Programmers;
+
+public class 문자열을정수로바꾸기 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public int solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}
